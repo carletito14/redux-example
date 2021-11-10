245980a215e47215f018c98ad9ae84ad606197d5
carletito14 <contactomanuelgutierrez@gmail.com>
2021-11-10 15:56:19 +0100

    redux example update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { reducer } from './features/todos';
+import {asyncMiddleware} from './middlewares/async'
+
+const store = createStore(reducer, applyMiddleware(asyncMiddleware))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
